9e3987e3efea24b34b94971b0ac57e7edca1c22d
BradenEberhard <50636857+BradenEberhard@users.noreply.github.com>
2023-07-05 15:17:07 -0600

    refactor and add game dashboard

--- dashboard/pages/Game_Dashboard.py
+++ dashboard/pages/Game_Dashboard.py
@@ -21,7 +21,8 @@ def main():
     teams_df = get_teams_df()
     with st.expander('Filters'):
         team_filter = st.selectbox('Team', [x.capitalize() for x in teams_df.teamID.unique() if 'allstar' not in x])
-        year_filter = st.selectbox('Year', sorted(teams_df[teams_df.teamID == team_filter.lower()].year))
+        team_filter = team_filter.lower()
+        year_filter = st.selectbox('Year', sorted(teams_df[teams_df.teamID == team_filter].year))
         team_games = games_df[(games_df.homeTeamID == team_filter) | (games_df.awayTeamID == team_filter)]
         st.write(team_games)
         team_games = team_games[team_games.startTimestamp.apply(lambda x:x[:4]) == year_filter]
