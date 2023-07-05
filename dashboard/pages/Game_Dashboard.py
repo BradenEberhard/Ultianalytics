3b8e48651e3b526e00061ce1a393307979808fa0
BradenEberhard <50636857+BradenEberhard@users.noreply.github.com>
2023-07-05 14:52:34 -0600

    refactor and add game dashboard

--- dashboard/pages/Game_Dashboard.py
+++ dashboard/pages/Game_Dashboard.py
@@ -1,7 +1,29 @@
 import streamlit as st
+from audl.stats.endpoints.games import Games
+from audl.stats.endpoints.teams import Teams
+
+@st.cache_data
+def get_games_df():
+    games = Games()
+    games_df = games.get_games()
+    return games_df
+
+@st.cache_data
+def get_teams_df():
+    teams = Teams()
+    teams_df = teams.get_teams()
+    return teams_df
 
 def main():
     st.title('Game Dashboard')
+
+    games_df = get_games_df()
+    teams_df = get_teams_df()
+    modification_container = st.container()
+    with modification_container:
+        with st.expander('Filters'):
+            team_filter = st.selectbox('Team(s)', [x.capitalize() for x in teams_df.teamID.unique() if 'allstar' not in x])
+            st.write(team_filter.lower())
 
 if __name__ == '__main__':
     main()
