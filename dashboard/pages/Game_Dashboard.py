513b1ef3347cfe6a394d4344c1bff47679b1d948
BradenEberhard <50636857+BradenEberhard@users.noreply.github.com>
2023-07-05 16:00:10 -0600

    refactor and add game dashboard

--- dashboard/pages/Game_Dashboard.py
+++ dashboard/pages/Game_Dashboard.py
@@ -2,11 +2,23 @@ import streamlit as st
 import pandas as pd
 from audl.stats.endpoints.games import Games
 from audl.stats.endpoints.teams import Teams
+from audl.stats.endpoints.gamestats import GameStats
 
 def get_name_from_id(row):
     date = pd.to_datetime(row.startTimestamp).date().strftime('%m/%d/%y')
     out_str = f'{row.awayTeamID.capitalize()} at {row.homeTeamID.capitalize()} on {date}'
     return out_str
+
+@st.cache_data
+def get_box_scores(gameID):
+    game_stats = GameStats(gameID)
+    return game_stats.get_box_scores()
+
+@st.cache_data
+def get_roster_stats(gameID):
+    game_stats = GameStats(gameID)
+    return game_stats.get_roster_stats()
+
 
 @st.cache_data
 def get_games_df():
@@ -36,6 +48,12 @@ def main():
         team_games = games_df[(games_df.homeTeamID == team_filter) | (games_df.awayTeamID == team_filter)]
         team_games = team_games[team_games.startTimestamp.apply(lambda x:int(x[:4])) == year_filter]
         game_filter = st.selectbox('Game', sorted(team_games.name, key= lambda x:x[-8:]))
+
+    st.write(game_filter)
+    game = games_df[games_df.name == game_filter]
+    st.write(get_roster_stats(game.gameID))
+
+
 
 if __name__ == '__main__':
     main()
