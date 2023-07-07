eb4d9b6e4712bf55289ffdabb3c8ae678379a7ea
BradenEberhard <50636857+BradenEberhard@users.noreply.github.com>
2023-07-06 18:55:44 -0600

    logos

--- dashboard/pages/Game_Dashboard.py
+++ dashboard/pages/Game_Dashboard.py
@@ -158,7 +158,7 @@ def plot_game(game_prob, gameID, features, max_length = 629):
     fig.layout.images = [dict(
         source=home_logo,
         xref="paper", yref="paper",
-        x=0, y=0,
+        x=0, y=0.1,
         sizex=0.2, sizey=0.2,
         xanchor="left", yanchor="bottom"
       )]
