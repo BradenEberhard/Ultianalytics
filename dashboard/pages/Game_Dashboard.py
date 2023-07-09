2e3fd51392a42e9b9ca7f3c21977707e15c32420
BradenEberhard <50636857+BradenEberhard@users.noreply.github.com>
2023-07-08 18:59:24 -0600

    add team stats

--- dashboard/pages/Game_Dashboard.py
+++ dashboard/pages/Game_Dashboard.py
@@ -314,6 +314,7 @@ def get_team_stats(cache):
         return row
 
     df = df.apply(get_score_string, args=(['oLine', 'dLine', 'redZone'],) ,axis=1)
+    df.index = [cache.homeTeamID, cache.awayTeamID]
     # df['Penalties'] = list(cache.penalties)
     return df
 
