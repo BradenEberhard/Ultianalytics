11d6db619d1849a74f60b284b618d626fa29a97f
BradenEberhard <50636857+BradenEberhard@users.noreply.github.com>
2023-07-09 18:51:52 -0600

    firestore

--- dashboard/Home.py
+++ dashboard/Home.py
@@ -27,6 +27,8 @@ def main():
             st.write("""This page is designed to visualize win probabilities for completed AUDL games. It uses an LSTM that considers various gameplay dynamics and features, such as thrower coordinates, possession details, game quarter, scores, and score difference.""")
         with st.expander("Throwing Direction"):
             st.write("""This page condenses each players throws into a polar histogram and shows usage, efficiency and trends in throwing.""")
-            
+        with st.expander("Game Dashboard Archive"):
+            st.write("""This page condenses each players throws into a polar histogram and shows usage, efficiency and trends in throwing.""")
+       
 if __name__ == '__main__':
     main()
