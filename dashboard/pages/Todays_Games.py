fc988ab8deb178acd414d0c7058c2bea47104f42
BradenEberhard <50636857+BradenEberhard@users.noreply.github.com>
2023-07-09 22:46:07 -0600

    timezone

--- dashboard/pages/Todays_Games.py
+++ dashboard/pages/Todays_Games.py
@@ -447,6 +447,7 @@ def main():
     data_cache, games_df, game_filter = DataCache(), get_games_df(), '<select>'
     with st.expander('Today\'s Game(s)'):
         today = pd.Timestamp.today().strftime('%Y-%m-%d')
+        st.write(today)
         games_df['dates'] = [pd.to_datetime(x).date().strftime('%Y-%m-%d') for x in games_df.startTimestamp]
         today_games = games_df[games_df.dates == today]
         game_filter = st.selectbox('Game', ['<select>'] + list(today_games['name']), 0)
