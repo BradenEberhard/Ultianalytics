4a350e7917d728181e1c0f883ad2ff603e1a0131
BradenEberhard <50636857+BradenEberhard@users.noreply.github.com>
2023-07-10 16:11:12 -0600

    roster stats

--- dashboard/pages/1_This_Weeks_Games.py
+++ dashboard/pages/1_This_Weeks_Games.py
@@ -420,10 +420,11 @@ def display_game(data_cache, games_df, game_filter):
         r_col.plotly_chart(shot_plot(data_cache.game_throws, False, data_cache.awayTeamID, 10, 15), use_container_width=True)
         plot_pulls(data_cache, l_col, r_col)
         with st.expander('Roster Stats'):
+            l2_col, r2_col = st.columns(2)
             write_stats = data_cache.roster_stats[data_cache.roster_stats.teamID == data_cache.homeTeamID].drop(['playerID','teamID'], axis=1).set_index('fullName')
-            l_col.write(write_stats[write_stats.pointsPlayed > 0])
+            l2_col.write(write_stats[write_stats.pointsPlayed > 0])
             write_stats = data_cache.roster_stats[data_cache.roster_stats.teamID == data_cache.awayTeamID].drop(['playerID','teamID'], axis=1).set_index('fullName')
-            r_col.write(write_stats[write_stats.pointsPlayed > 0])
+            r2_col.write(write_stats[write_stats.pointsPlayed > 0])
         
         # print_logos(data_cache)
         # col1, col2 = st.columns(2)
