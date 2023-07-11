d7c3f06960ca1d20b3862aabb811716fb929ff3b
BradenEberhard <50636857+BradenEberhard@users.noreply.github.com>
2023-07-11 16:03:33 -0600

    continuous refresh button

--- dashboard/Home.py
+++ dashboard/Home.py
@@ -23,13 +23,14 @@ def main():
     st.caption("Braden Eberhard, braden.ultimate@gmail.com, 2023")
     st.subheader("Overview")
     st.write("""This dashboard is my naive attempt at visualizing and analyzing the information available through the AUDL's API. """)
+    with st.expander("Game Dashboards"):
+        st.write("""'This Weeks Games' and 'Game Dashboard Archive' both use a similar dashboard to display a games breakdown. This includes: team stats, pulls, win probability, and shot charts.""")
+      
     with st.expander("Win Probability Archive"):
         st.write("""This page is designed to visualize win probabilities for completed AUDL games. It uses an LSTM that considers various gameplay dynamics and features, such as thrower coordinates, possession details, game quarter, scores, and score difference.""")
     with st.expander("Throwing Direction"):
         st.write("""This page condenses each players throws into a polar histogram and shows usage, efficiency and trends in throwing.""")
-    with st.expander("Game Dashboard Archive"):
-        st.write("""'This Weeks Games' and 'Game Dashboard Archive' both use a similar dashboard to display a games breakdown. This includes: team stats, pulls, win probability, and shot charts.""")
-       
+ 
 if __name__ == '__main__':
     streamlit_analytics.start_tracking()
     main()
